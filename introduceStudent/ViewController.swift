94ef763691130c31cea9c04ad40e2f280b463fc1
Vaibhava <vaibhava.potturu@gmail.com>
2022-12-28 17:37:53 -0500

    Initial Commit

---
+++ introduceStudent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  introduceStudent
+//
+//  Created by Vaibhava Potturu on 12/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
